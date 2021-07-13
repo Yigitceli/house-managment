38f0ec6f4daaeca9dd18ae7b203fe8451a3c8fa1
Yigitceli <yigit.celi@outlook.com>
2021-07-13 23:47:26 +0300

    nav items configured

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
 import './Header.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 function Header() {
 
@@ -14,8 +20,23 @@ function Header() {
     return (
         <div>
             <header className="App-header">
-                <div className="nav-icon"><i class="fas fa-bars" onClick={navBarChanger}></i></div>
-                <div className={navBar}></div>
+                <div className="nav-icon">{active ? <i class="fas fa-times" onClick={navBarChanger}></i> : <i class="fas fa-bars" onClick={navBarChanger}></i>}</div>
+                <div className={navBar}>
+                    <ul>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Daire ve Müşteri Ekle</li></Link></div>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Daire Kiralama/Satın Alım</li></Link></div>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Daire Listele</li></Link></div>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Müşteri Listele</li></Link></div>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Daire Bilgileri Görüntüle</li></Link></div>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Daire Güncelle</li></Link></div>
+                        <div className="link-div"><Link className="link"><li><i class="fas fa-angle-double-right"></i>Müşteri Güncelle</li></Link></div>
+                        
+                        
+                    
+                    
+                    
+                    </ul>
+                </div>
                 <h1>Konut Yönetim Sistemi</h1>
             </header>
         </div>
